e6482abc86a53c843d8ba811bf352da3f764065c
Henry Wang <henrywang@Henrys-MacBook-Pro-3.local>
2023-07-01 22:28:55 +1200

    feat: add hero section to index.rs

--- src/templates/index.rs
+++ src/templates/index.rs
@@ -1,25 +1,12 @@
 use perseus::prelude::*;
 use sycamore::prelude::*;
 
-use crate::components::header::Header;
+use crate::components::{header::Header, hero::Hero};
 
 fn index_page<G: Html>(cx: Scope) -> View<G> {
     view! { cx,
-        Header {}
-        main {
-            p {
-                "Hi Hello! I'm Henry Wang, a penultimate year software engineering student at the University of Auckland."
-                br {}
-                br {}
-                "As you can tell, the site here is still very WIP (I'm learning "
-                a (href="https://framesurge.sh/perseus") {"Perseus"}
-                " to create this website). In the meantime, feel free to check out my "
-                a (href="https://www.linkedin.com/in/henry-h-wang/") {"Linkedin"}
-                " or contact me via email at  "
-                a (href="mailto:wang.henryhan@gmail.com"){ "wang.henryhan@gmail.com" }
-                "."
-            }
-        }
+        Header
+        main { Hero }
     }
 }
 
