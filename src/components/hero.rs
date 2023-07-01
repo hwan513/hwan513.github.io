3d22b54f58757aba8beb9b205f3ee2a1edfa6914
Henry Wang <henrywang@Henrys-MacBook-Pro-3.local>
2023-07-01 22:28:19 +1200

    feat: add component for hero section

---
+++ src/components/hero.rs
@@ -0,0 +1,24 @@
+use sycamore::prelude::*;
+
+#[component]
+pub fn Hero<G: Html>(cx: Scope) -> View<G> {
+    view!(cx,
+        section (id="hero") {
+            div (id="heroWrapper"){
+                div (id="heroText") {
+                    p { "Hi there, I'm" }
+                    div (id="heroName") { "Henry Wang"}
+                    p { "I am a penultimate software engineering student at
+                    the University of Auckland, with a passion to explore 
+                    everything that can be offered by software technologies."}
+                    div (id="icons") {
+
+                    }
+                }
+                div (id="heroImage") {
+                    img (src="", alt="Photo of Henry Wang")
+                }
+            }
+        }
+    )
+}
