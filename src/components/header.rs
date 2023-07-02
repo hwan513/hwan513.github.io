11351967ece7fd7d043093f929fb9b6f5f777d98
Henry Wang <henrywang@Henrys-MacBook-Pro-3.local>
2023-07-02 23:28:40 +1200

    fix: return to top nav uses "#" instead of "#hero"

--- src/components/header.rs
+++ src/components/header.rs
@@ -10,7 +10,7 @@ fn NavLinks<G: Html>(cx: Scope) -> View<G> {
     view!(cx,
         nav {
             ul {
-                li { a(href="#hero") {"Home"} }
+                li { a(href="#") {"Home"} }
                 li { a(href="#about") {"About Me"} }
                 li { a(href="#projects") {"Projects"} }
                 li { a(href="#contact") {"Contact"} }
