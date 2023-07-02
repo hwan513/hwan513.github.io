b1bda17cd5139eae28890da97863a5fa8b139583
Henry Wang <henrywang@Henrys-MacBook-Pro-3.local>
2023-07-02 15:28:00 +1200

    feat: attempt to add CV hosted in a relative path

--- src/main.rs
+++ src/main.rs
@@ -9,4 +9,5 @@ pub fn main<G: Html>() -> PerseusApp<G> {
     PerseusApp::new()
         .template(crate::templates::index::get_template())
         .error_views(crate::error_views::get_error_views())
+        .static_alias("/CV.pdf", "static/files/CV.pdf")
 }
