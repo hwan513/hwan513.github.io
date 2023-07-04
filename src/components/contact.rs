67b382b6fce60a06b57a12ac7f30caf35afe58a5
Henry Wang <henrywang@Henrys-MacBook-Pro-3.local>
2023-07-04 21:29:54 +1200

    feat: add UI for contact form

--- src/components/contact.rs
+++ src/components/contact.rs
@@ -1,16 +1,88 @@
 use sycamore::prelude::*;
 
+#[cfg(client)]
+use web_sys::window;
+
 #[component]
 pub fn Contact<G: Html>(cx: Scope) -> View<G> {
     view!(cx,
         section (id="contact", style="min-height: 40vh;") {
-            h1 { "Contact"}
-            p { "There will be a form here where you can send an email to me. In the meantime, feel free to check out my "
-                a (href="https://www.linkedin.com/in/henry-h-wang/", target="_blank") {"Linkedin"}
-                " or contact me via email at  "
-                a (href="mailto:mail@henryh.wang"){ "mail@henryh.wang" }
-                "."
+            h1 { "Contact Me 👋"}
+            p { "Fill the form to send me an email." }
+            form {
+                ContactForm(FieldProps::new("Full Name", "text", "contactName"))
+                ContactForm(FieldProps::new("Email", "email", "contactEmail"))
+                ContactForm(FieldProps::new("Subject", "text", "contactSubject"))
+                ContactMessage
+                ContactForm(FieldProps::new("", "submit", "Send"))
             }
+        }
+    )
+}
+
+#[derive(Prop)]
+struct FieldProps {
+    field_label: String,
+    field_type: String,
+    field_name: String,
+}
+
+impl FieldProps {
+    fn new(field_label: &str, field_type: &str, field_name: &str) -> Self {
+        Self {
+            field_label: field_label.to_string(),
+            field_type: field_type.to_string(),
+            field_name: field_name.to_string(),
+        }
+    }
+}
+
+#[component]
+fn ContactForm<G: Html>(cx: Scope, props: FieldProps) -> View<G> {
+    let FieldProps {
+        field_name,
+        field_label,
+        field_type,
+    } = props;
+    let field_name_clone = field_name.clone();
+    let field_name_clone_clone = field_name.clone();
+
+    view!(cx,
+        div(class="contact-field") {
+            label (for=field_name){ (field_label) }
+            input (type=field_type, name=field_name_clone, id=field_name_clone_clone, required=true)
+        }
+    )
+}
+
+#[component]
+fn ContactMessage<G: Html>(cx: Scope) -> View<G> {
+    let message_height = create_signal(cx, 0);
+
+    // TODO: see if I can actually use the node_ref
+    // let node_ref = create_node_ref(cx);
+
+    let input_handler = move |_| {
+        #[cfg(client)]
+        {
+            let document = window().unwrap().document().unwrap();
+            let contact_message = document.get_element_by_id("contactMessage").unwrap();
+            contact_message.scroll_height();
+            message_height.set(0);
+            message_height.set(contact_message.scroll_height());
+        }
+    };
+
+    view!(cx,
+        div(class="contact-field") {
+            label(for="contactMessage") { "Message" }
+            textarea(
+                // ref=node_ref,
+                on:input=input_handler,
+                style=format!("height: calc({}px - 1em)", message_height.get()),
+                name="contactMessage",
+                id="contactMessage",
+                required=true)
         }
     )
 }
