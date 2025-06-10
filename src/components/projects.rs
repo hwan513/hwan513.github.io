e6f38ea4174b067734f76938899cf91081adb18a
Henry Wang <wang.henryhan@gmail.com>
2025-06-11 08:25:12 +1200

    feat: add macro to re-use code for projects

--- src/components/projects.rs
+++ src/components/projects.rs
@@ -1,11 +1,27 @@
 use leptos::prelude::*;
 
+/// This is so cursed I swear
+macro_rules! project {
+    ($md_path:literal, $img_path:literal, $alt:literal) => {
+        view! {
+            <div class="project">
+                <picture>
+                    <source srcset=concat!($img_path, ".webp") type="image/webp" />
+                    <img src=concat!($img_path, ".png") alt=$alt />
+                </picture>
+                <article inner_html=markdown::to_html(include_str!($md_path))></article>
+            </div>
+        }
+    };
+}
+
 /// Renders the About section
 #[component]
 pub fn Projects() -> impl IntoView {
     view! {
-        <section id="projects">
-            <h1>"Projects"</h1>
+        <section id="projects" class="projects">
+            <h1>"Projects 💼"</h1>
+            {project!("../markdown/about.md", "../images/painting", "Painting")}
         </section>
     }
 }
